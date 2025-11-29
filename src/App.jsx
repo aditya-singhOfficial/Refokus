22b31b8d95761fd4743132e9bfa9b1c7b18daff7
Aditya Singh <1211821022@ngpp.ac.in>
2025-11-30 02:09:14 +0530

    Initialize project setup and configurations

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/nav/Navbar.jsx";
+import Work from "./components/hero/Work.jsx";
+import Stripes from "./components/stripess/Stripes.jsx";
+import Products from "./components/products/Products.jsx";
+import Marquess from "./components/marque/Marquess.jsx";
+
+const App = () => {
+  return (
+    <div className="w-full select-none  bg-zinc-900 text-white">
+      <Navbar />
+      <Work />
+      <Stripes />
+      <Products />
+      <Marquess />
+    </div>
+  );
+};
+
+export default App;
